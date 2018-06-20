79f47579130dd3f0ac85abfe782decf68650b801
Theresa Campbell <t.campbell@sasktel.net>
2018-06-20 22:05:54 +0000

    Tweets post to the Tweeter main page and the textarea clears when the 'Tweet' button is pushed. This is done without refreshing the page.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -50,11 +50,40 @@ function loadTweets() {
     url: '/tweets',
     success: function (moreTweetsJson){
       renderTweets(moreTweetsJson);
+      $(".tweet-input").val('');
+      $(".counter").text(140);
     }
   })
 };
 
 
+function submitTweetViaAjax(data) {
+
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: data
+    }).done(function () {
+        loadTweets();
+  })
+}
+
+
+
 $(document).ready(function() {
+
   loadTweets();
+
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+    let $textInTweet = $(this).children(".tweet-input")
+    if ($textInTweet.val().length > 140) {
+      alert("Tweet is too long. Shorten to at least 140 characters.")
+    } else if ($textInTweet.val().length === 0) {
+      alert("Can't submit an empty tweet. Write something!")
+    } else {
+      let formData = $(this).children(".tweet-input").serialize();
+      submitTweetViaAjax(formData);
+    }
+  })
 });
--- public/scripts/composer-submit-functionality.js
+++ public/scripts/composer-submit-functionality.js
@@ -1,27 +1,31 @@
-function submitTweetViaAjax(data) {
+// function submitTweetViaAjax(data) {
 
-    $.ajax({
-      method: 'POST',
-      url: '/tweets',
-      data: data
-    })
-}
+//     $.ajax({
+//       method: 'POST',
+//       url: '/tweets',
+//       data: data
+//     }).done(function {
+
+//     })
+// }
 
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  $("form").on("submit", function (event) {
-    event.preventDefault();
+//   $("form").on("submit", function (event) {.
+//     event.preventDefault();
 
-    let $textInTweet = $(this).children(".tweet-input")
+//     let $textInTweet = $(this).children(".tweet-input")
 
-    if ($textInTweet.val().length > 140) {
-      alert("Tweet is too long. Shorten to at least 140 characters.")
-    } else if ($textInTweet.val().length === 0) {
-      alert("Can't submit an empty tweet. Write something!")
-    } else {
-      let formData = $(this).children(".tweet-input").serialize();
-      submitTweetViaAjax(formData);
-    }
-  })
-});
+//     if ($textInTweet.val().length > 140) {
+//       alert("Tweet is too long. Shorten to at least 140 characters.")
+//     } else if ($textInTweet.val().length === 0) {
+//       alert("Can't submit an empty tweet. Write something!")
+//     } else {
+//       let formData = $(this).children(".tweet-input").serialize();
+//       submitTweetViaAjax(formData);
+//     }
+//   })
+// });
+
+//REFACTOR LATER FROM APP.JS
